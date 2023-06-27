2923c2f0997bc48eb5c59feba251bc4f203b9f9d
monkeycs60 <clement.serizay@gmail.com>
2023-06-27 23:53:09 +0200

    add react strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
@@ -5,11 +6,11 @@ import './index.css';
 import { Providers } from './redux/provider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	// <React.StrictMode>
+	<React.StrictMode>
 		<BrowserRouter>
 			<Providers>
 				<App />
 			</Providers>
 		</BrowserRouter>
-	// </React.StrictMode>
+	</React.StrictMode>
 );

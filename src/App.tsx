987061ae6ee6dad0cb2d2af2e37a5ba6fb9a3488
monkeycs60 <clement.serizay@gmail.com>
2023-06-28 16:58:02 +0200

    refactor: add suspense and lazy for perf

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
+import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Index from './pages/Index';
-import EmployeeList from './pages/EmployeeList/EmployeeList.tsx';
+// import Index from './pages/Index';
+// import EmployeeList from './pages/EmployeeList/EmployeeList.tsx';
+
+const Index = lazy(() => import('./pages/Index'));
+const EmployeeList = lazy(() => import('./pages/EmployeeList/EmployeeList.tsx'));
 
 function App() {
 	return (
 		<Routes>
-			<Route path='/' element={<Index />} />
-			<Route path='/employee-list' element={<EmployeeList />} />
+			<Route
+				path='/'
+				element={
+					<Suspense fallback={<div>Loading...</div>}>
+						<Index />
+					</Suspense>
+				}
+			/>
+			<Route
+				path='/employee-list'
+				element={
+					<Suspense fallback={<div>Loading...</div>}>
+						<EmployeeList />
+					</Suspense>
+				}
+			/>
 		</Routes>
 	);
 }
